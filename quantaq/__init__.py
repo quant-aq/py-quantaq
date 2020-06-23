f528837130ea00c44d61c85305b463e177c3615c
David H Hagan <david@davidhhagan.com>
2020-06-23 12:50:45 -0700

    switched to new version getter

--- quantaq/__init__.py
+++ quantaq/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution
+from getversion import get_module_version
+import quantaq
+
+__version__, _ = get_module_version(quantaq)
 
 from .baseapi import TokenError, NotFoundError, NotPermittedError, \
     BadRequestError
 
 from .legacy import *
 
-__version__ = get_distribution("opcsim").version
